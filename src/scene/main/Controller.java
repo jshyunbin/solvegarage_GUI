891db00fcc5f6749105f92c2a5d7a96166cb84d9
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2019-06-03 19:45:52 +0900

    initial commit

---
+++ src/scene/main/Controller.java
@@ -0,0 +1,57 @@
+package scene.main;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+    @FXML Label loginLabel, solvegarage;
+    @FXML Pane leftBorder;
+    @FXML Button signupBtn;
+    @FXML TextField searchfield;
+    @FXML ScrollPane contentPane;
+
+    // TODO: make FXMLs files for each tab
+    MultiScreen contentScreen = new MultiScreen();
+    private Parent leftTab;
+    private Parent loginpage;
+    String currentPage = "discover";
+    // TODO: add screens to contentScreen
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        try{
+            leftTab = FXMLLoader.load(getClass().getResource("../../FXMLs/leftTab.fxml"));
+            loginpage = FXMLLoader.load(getClass().getResource("../../FXMLs/loginPage.fxml"));
+        } catch(IOException e) {
+            System.out.println("No file named \"../../FXMLs/leftTab.fxml\" or \"../../FXMLs/loginPage.fxml\"");
+        }
+        leftBorder.getChildren().add(leftTab);
+        contentPane.setContent(contentScreen.currentScreen());
+        loginLabel.setOnMouseClicked(e -> loginClick());
+        solvegarage.setOnMouseClicked(e -> iconClick());
+    }
+
+    private void loginClick() {
+        currentPage = "loginPage";
+        contentPane.setContent(loginpage);
+        System.out.println("loginPage");
+    }
+
+    private void iconClick() {
+        currentPage = "discover";
+        System.out.println("discover");
+    }
+
+
+}
