05629a9b010066462671a02decc0483bcfb5f51d
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2019-06-18 12:00:54 +0900

    test Class fixed

--- src/src/test/Test.java
+++ src/src/test/Test.java
@@ -3,16 +3,16 @@ package src.test;
 import com.google.gson.JsonObject;
 import src.Utils.RSA;
 import src.Utils.SecureHttpConnection;
-import src.main.Main;
-import src.scene.main.Controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ConnectException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.Objects;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         byte[] serverPublicKey = null;
         try {
             serverPublicKey = SecureHttpConnection.getServerPublicKey();
@@ -41,19 +41,24 @@ public class Test {
             int code = object.get("code").getAsInt();
             if (code == 1) {
                 System.out.println("username is wrong");
-            }
-            else if (code == 2) {
+            } else if (code == 2) {
                 System.out.println("password is wrong");
             }
         }
+        JsonObject root = null;
+        File file = new File("test.png");
+        FileInputStream in = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        //noinspection ResultOfMethodCallIgnored
+        in.read(data);
+        String img = Base64.getEncoder().encodeToString(data);
         try {
-            String content = new String(Files.readAllBytes(Paths.get("C:\\Users\\Joshua Lee\\Documents\\이현빈\\경기영재과학고등학교\\정보과학\\정보과학 프로젝트I\\IntelliJ-workspace\\SolveGarage\\src\\src\\test\\image.txt")));
-            System.out.println(content);
-            System.out.println(content.length());
-            SecureHttpConnection.post(SecureHttpConnection.profileImgURL, "{'id':'test','token':"+token+","+content,
-                    serverPublicKey, RSA.generateKeyPair());
+            root = SecureHttpConnection.post("http://localhost:3080/profile-image", "{'id':'test','token':'" + token + "','img':'" + img + "'}", serverPublicKey, Objects.requireNonNull(RSA.generateKeyPair()));
         } catch (Exception e) {
             e.printStackTrace();
         }
+        assert root != null;
+        token = root.get("token").getAsString();
+        assert root.get("success").getAsBoolean();
     }
 }
